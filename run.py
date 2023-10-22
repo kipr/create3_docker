bcde6c1e5bdb95eb3588894a10c73e9ad3d5aa21
Braden McDorman <bmcdorman@gmail.com>
2023-10-22 03:55:51 +0000

    read IP from env

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import argparse
 import subprocess
+from os import environ
 
 # Get home path
 HOME_PATH = Path.home()
@@ -9,22 +10,14 @@ FASTDDS_TEMPLATE_PATH = HOME_PATH / "fastdds" / "fastdds.xml.template"
 FASTDDS_FINAL_PATH = HOME_PATH / "fastdds.xml"
 CREATE3_SERVER = HOME_PATH / "create3" / "build" / "server" / "create3_server"
 
-parser = argparse.ArgumentParser(description="Run")
 
-parser.add_argument(
-  "--ip",
-  help="Local IP",
-  required=True
-)
-
-args = parser.parse_args()
-
+ip = environ.get("IP")
 
 # Create the fastdds config file from template
 with open(FASTDDS_TEMPLATE_PATH, "r") as f:
   fastdds_template = f.read()
 
-fastdds_config = fastdds_template.format(ip=args.ip)
+fastdds_config = fastdds_template.format(ip=ip)
 
 with open(FASTDDS_FINAL_PATH, "w") as f:
   f.write(fastdds_config)
@@ -33,9 +26,10 @@ env = {
   "FASTDDS_DEFAULT_PROFILES_FILE": FASTDDS_FINAL_PATH.as_posix()
 }
 
+
 # Run "fastdds discovery -i 0 -l {ip} -p 11811" in a subprocess asynchronously
 subprocess.Popen(
-  ["fastdds", "discovery", "-i", "0", "-l", args.ip, "-p", "11811"],
+  ["fastdds", "discovery", "-i", "0", "-l", ip, "-p", "11811"],
   env=env
 )
 
